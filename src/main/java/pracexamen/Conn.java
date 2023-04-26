4c01ea23457e88af8cb4746a24330985c0247421
unknown <enriquefloreslama@gmail.com>
2023-04-26 01:01:30 -0500

    añadido conn

--- src/main/java/pracexamen/Conn.java
+++ src/main/java/pracexamen/Conn.java
@@ -1,5 +1,24 @@
 package pracexamen;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import pracexamen.Conn;
+
 public class Conn {
 
+	private static Conn instancia = new Conn();
+	private EntityManagerFactory factory;
+	
+	public Conn() {
+		this.factory = Persistence.createEntityManagerFactory("pracexamen");
+	}
+	
+	public static Conn getInstancia() {
+		return instancia;
+	}
+	
+	public EntityManagerFactory getFactory() {
+		return factory;
+	}
 }
